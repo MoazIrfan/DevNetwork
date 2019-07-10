c1a1eed0388bb2ac2401667990efa928b4366c22
Moaz Irfan <irfanmoaz@gmail.com>
2019-07-10 14:10:19 +0500

    Update Navbar.js

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -62,7 +62,7 @@ class Navbar extends Component {
     );
 
     return (
-      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+      <nav className="navbar navbar-expand-sm navbar-light bg-white">
         <div className="container">
           <Link className="navbar-brand" to="/">
             DevNetwork
